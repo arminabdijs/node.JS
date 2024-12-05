d44da12c7ec4543b3a11539a8c3490fe8fa5d51b
arminabdijs <arminabdijs@gmail.com>
2024-12-05 20:03:17 +0330

    first push

---
+++ 1-overviewOfJS/1-Variable/index.js
@@ -0,0 +1,32 @@
+let age = 25;
+
+let name = "John Doe";
+
+let isEmployed = true;
+
+
+console.log("Name:", name);
+
+
+console.log("Age:", age);
+
+
+console.log("Is employed:", isEmployed);
+
+
+//----------------------------------------------------------------
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof isEmployed);
+
+//----------------------------------------------------------------
+let age1 = 25;
+
+var name1 = "John Doe";
+
+const isEmployed1 = true;
+
+//----------------------------------------------------------------
---
+++ 1-overviewOfJS/2-MathematicalOperators/index.js
@@ -0,0 +1,54 @@
+// Mathematical Operators for numbers
+let number1 = 35
+let number2 = 23
+
+console.log(number1 + number2)
+
+console.log(number1 - number2)
+
+console.log(number1 * number2)
+
+console.log(number1 / number2)
+
+console.log(number1 % number2)
+
+console.log(number1 ** number2)
+
+// ----------------------------------------------------------------
+// Not a Number
+let raminUserName = "Ramin"
+let arminUserName = "Armin"
+
+console.log(raminUserName - arminUserName)
+
+console.log(raminUserName / arminUserName)
+
+console.log(raminUserName * arminUserName)
+
+console.log(raminUserName ** arminUserName)
+
+console.log(raminUserName % arminUserName)
+
+console.log(raminUserName + arminUserName)
+
+console.log(raminUserName + " " + arminUserName)
+
+
+let userAge = "Ten"
+
+console.log(userAge + 10)
+
+// ----------------------------------------------------------------
+// convert string to number
+let userScore = "75"
+
+console.log(userScore + 10)
+
+console.log(Number(userScore) + 10)
+
+
+let userID =+"998"
+
+console.log(userID + 2)
+
+
