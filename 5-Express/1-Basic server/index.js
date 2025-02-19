c664a98885ac0c4fec81d10f740014bd507b39cc
arminabdijs <arminabdijs@gmail.com>
2025-02-19 08:08:20 +0330

    add file express in project

---
+++ 5-Express/1-Basic server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/home", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+})
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+})
+
+app.get("/products", (req, res) => {
+  res.send("Products Page");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ 5-Express/3-Response/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const courses = [
+  { id: 1, name: "JS", price: 100 },
+  { id: 2, name: "Node", price: 200 },
+  { id: 3, name: "React", price: 300 },
+  { id: 4, name: "Express", price: 0 },
+];
+
+/* app.get("/courses/:id", (req, res) => {
+  res.send(req.params.id);
+}); */
+app.get("/courses/:id", (req, res) => {
+  /*   const course = courses.find((course) => course.id === Number(req.params.id));*/
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+  if (!course)
+    res.status(404).send("The course with the given ID was not found");
+  res.send(course);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
